9df464bccdea1785f72a2ef31bc49c7c9979d5b7
fezin <2411550401@fema.edu.br>
2025-05-17 11:43:52 -0300

    fix: add validation at passwords field

--- src/types/schemas/authSchemas.ts
+++ src/types/schemas/authSchemas.ts
@@ -4,7 +4,9 @@ const loginRequestSchema = z.object({
   login: z.string({ required_error: "Preencha o campo usuário" }).min(5, {
     message: "Campo usuário deve conter ao menos 5 dígitos",
   }),
-  password: z.string({ required_error: "Preencha o campo senha" }),
+  password: z
+    .string({ required_error: "Preencha o campo senha" })
+    .min(1, { message: "Preencha o campo senha" }),
 });
 
 export { loginRequestSchema };

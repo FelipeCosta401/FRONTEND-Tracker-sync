56a8a3a02555ee2ea87fe3a0c56609752269990b
fezin <2411550401@fema.edu.br>
2025-05-20 15:56:54 -0300

    feat: add stored token at api headers

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -1,7 +1,12 @@
 import axios from "axios"
 
+const token = localStorage.getItem("token")
+
 const Api = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: "http://localhost:8080/api",
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
 })
 
 export default Api
